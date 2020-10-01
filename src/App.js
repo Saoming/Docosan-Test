a0eae2aa45f1c73f9aa12e218da8e372f32f95c7
Samuel Tirtawidjaja <Hujanesword@gmail.com>
2020-10-01 16:41:59 +0700

    cleaning some spaces and unused import

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component} from 'react';
 import Archive from "./components/archive"
 
 class App extends Component {
   render() {
     return (
-      <Fragment>
+      <>
         <h1> Danh sach cac bac si</h1>
         <Archive />
-      </Fragment>
+      </>
     );
   }
 }
